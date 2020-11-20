37cccd7e18c8db681fb2952397b90db4e246ea89
gokayaking <brunjest@gmail.com>
2020-11-20 16:05:13 -0500

    Update NBA_AllTeams_MeanCompare.py

--- NBA_AllTeams_MeanCompare.py
+++ NBA_AllTeams_MeanCompare.py
@@ -1,10 +1,13 @@
-import csv
-import os
-
 import pandas as pd 
-from scipy import stats
 
-pd.set_option('display.max_columns', None)
+
+# do T Test and Log
+def doMeanCompare( df_before, df_precovid, df_after, columnName ): 
+    print( "\n" )
+    print( "2018-2019           ", column, "mean for all teams", df_before[column].mean() )
+    print( "2018-2019 pre-covid ", column, "mean for all teams", df_precovid[column].mean() )
+    print( "2019-2020 covid     ", column, "mean for all teams", df_after[column].mean() )
+
 
 compareColumn = "FG%"
 filename = "NBA Team Data Game Logs.xlsx"
@@ -15,44 +18,14 @@ post_df=df[df.Date>="07/30/2020"]
 pre_2018_2019_df = pre_df[pre_df.Season == "2018-19"]
 pre_2019_2020_df = pre_df[pre_df.Season == "2019-20"]
 
-#pre_2018_2019_df_fgpercent_mean = pre_2018_2019_df.mean()
-#pre_2019_2020_df_fgpercent_mean = pre_2019_2020_df.mean()
-#post_df_fgpercent_mean = post_df.mean()
-
-#print( " 2018-2019 all teams mean", pre_2018_2019_df_fgpercent_mean)
-#print( " 2019-2020 all teams mean", pre_2019_2020_df_fgpercent_mean)
-#print( " 2019-2020 covid all teams mean", post_df_fgpercent_mean)
 
 
-# pre_2018_2019_df_five = pre_2018_2019_df.describe() 
-# pre_2019_2020_df_five = pre_2019_2020_df.describe() 
-
-# print( " pre_df_lal_five ", pre_2018_2019_df_five )
-# print( " post_df_lal_five ", pre_2019_2020_df_five )
-
-print( "\n" )
-
-all_2018_2019_fg_percent_mean = pre_2018_2019_df["FG%"].mean()
-print( "2018-2019 FG% mean for all teams", all_2018_2019_fg_percent_mean)
-
-all_2019_2020_fg_percent_mean = pre_2019_2020_df["FG%"].mean()
-print( "2019-2020 pre-covid FG% mean for all teams", all_2019_2020_fg_percent_mean)
-
-all_2019_2020_covid_fg_percent_mean = post_df["FG%"].mean()
-print( "2019-2020 covid FG% mean for all teams", all_2019_2020_covid_fg_percent_mean)
-
-print( "\n" )
+columns = ['FG%', 'FT', 'FTA', 'FT%', 'DRtg', 'FTr', 'TS%', 'eFG%', 'FT/FGA']
+for column in columns:
+    doMeanCompare( pre_2018_2019_df, pre_2019_2020_df, post_df, column )
 
 
-all_2018_2019_dvtg_percent_mean = pre_2018_2019_df["DRtg"].mean()
-print( "2018-2019 DRtg mean for all teams", all_2018_2019_dvtg_percent_mean)
 
-all_2019_2020_dvtg_percent_mean = pre_2019_2020_df["DRtg"].mean()
-print( "2019-2020 pre-covid DRtg mean for all teams", all_2019_2020_dvtg_percent_mean)
 
-all_2019_2020_covid_dvtg_percent_mean = post_df["DRtg"].mean()
-print( "2019-2020 covid DRtg mean for all teams", all_2019_2020_covid_dvtg_percent_mean)
-
-print( "\n" )
 
 

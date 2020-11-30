78ae4adbcba32581c01dc1c4e556c9e8d2e67f4d
gtrail5 <73748234+gtrail5@users.noreply.github.com>
2020-11-29 19:11:23 -0500

    Update to code and data

--- Analyses/PlayerCorrTable.py
+++ Analyses/PlayerCorrTable.py
@@ -13,36 +13,51 @@ import pandas as pd
 from scipy import stats
 
 os.getcwd()
-os.chdir("../data/")
+os.chdir("/Users/gtrailv5/CS504/GitHub/team-eta/data")
 os.getcwd()
 
-playerData= pd.read_csv("NBA Player Game Log Data.csv")
+playerData= pd.read_excel("NBA Player Game Log Data.xlsx")
 
-del playerData['Unnamed: 13']
-del playerData['Unnamed: 14']
+prePlayer= playerData[playerData.Date<"07/30/2020"]
+print(prePlayer)
+postPlayer= playerData[playerData.Date>'07/30/2020']
+print(postPlayer)
+"""del playerData['Unnamed: 13']
+del playerData['Unnamed: 14']"""
 
-print(playerData)
+"""print(playerData)"""
 
 playerData= playerData.dropna()
+prePlayer= prePlayer.dropna()
+postPlayer= postPlayer.dropna()
 
 print(playerData)
+print(prePlayer)
+print(postPlayer)
 
-playerNominal=['Season','Player','Date','Home/Away','Opp','Win/Loss']
+players= ['Aaron Gordon','CJ McCollum','Dennis Schroder','Donovan Mitchell','Dorian Finney-Smith','Doug McDermott','Evan Fournier','Jakob Poeltl','Jarrett Allen','Joe Ingles','Montrezl Harrell','Myles Turner','Nerlens Noel','Rudy Gay','Terrence Ross']
 
-playerNumeric= playerData
+playerNominal=['Season','Player','Date','Home/Away','Opp','Win/Loss','Tm']
+
+#playerNumeric= playerData
+preNumeric= prePlayer
+#postNumeric= postPlayer
 
 for nom in playerNominal:
-    del playerNumeric[nom]
+   # del playerNumeric[nom]
+    del preNumeric[nom]
+   # del postNumeric[nom]
+del preNumeric['Tm']
 
+print(preNumeric)
 
-print(playerNumeric)
+#corrMatrix= playerNumeric.corr()
+preMatrix= preNumeric.corr()
 
-corrMatrix= playerNumeric.corr()
-
-print(corrMatrix)
+print(preMatrix)
 
 import seaborn as sn
 import matplotlib.pyplot as plt
 
-sn.heatmap(corrMatrix, annot=True)
+sn.heatmap(preMatrix, annot=True)
 plt.show()

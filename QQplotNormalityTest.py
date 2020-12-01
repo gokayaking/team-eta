fac35026dcec18284a5a7bb9e71aaf6cb22afe7b
gokayaking <brunjest@gmail.com>
2020-12-01 15:14:50 -0500

    fixed typo with post_COVID and renamed to remove spaces

--- QQplotNormalityTest.py
+++ QQplotNormalityTest.py
@@ -5,18 +5,16 @@ Created on Tue Dec  1 10:59:13 2020
 @author: johnn
 """
 import pandas as pd
-import scipy.stats as stats
 from statsmodels.graphics.gofplots import qqplot
 import matplotlib.pyplot as plt
-import os
 
 pd.set_option('display.max_columns', None)
 
 filename = "NBA Team Data Game Logs.xlsx" # Team level data
 #filename = "NBA Player Game Log Data.xlsx" #Player level data
 data = pd.read_excel( "./data/" + filename )
-pre_COVID = data[data.Date<"07/30/2020"]
-post_COVID = data[data.Date>"07/30/2020"]
+pre_COVID = data[data.Date < "07/30/2020"]
+post_COVID = data[data.Date >= "07/30/2020"]
 
 stats = ['FG%', 'FT', 'FTA', 'FT%', 'DRtg', 'FTr', 'TS%', 'eFG%', 'FT/FGA']
 
@@ -26,6 +24,6 @@ for i in range(9):
     plt.show()
     
 for i in range(9):
-    qqplot(Post_COVID[stats[i]], line = 's')
+    qqplot(post_COVID[stats[i]], line = 's')
     plt.title('Post Covid ' + stats[i])
     plt.show()

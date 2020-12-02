7d19fa5de9e7a34bff16a4b25f15b7681ba4f0fe
gokayaking <brunjest@gmail.com>
2020-12-02 09:42:16 -0500

    Revert "Merge branch 'main' of https://github.com/gokayaking/team-eta into main"
    This reverts commit 67cebc894ff5a22133d25b2e782d8b237100723d, reversing
    changes made to a2a707c7624510a02c61b3a536ef9dd16dfd657a.

--- Analyses/PlayerCorrTable.py
+++ Analyses/PlayerCorrTable.py
@@ -13,50 +13,36 @@ import pandas as pd
 from scipy import stats
 
 os.getcwd()
-os.chdir("/Users/gtrailv5/CS504/GitHub/team-eta/data")
+os.chdir("../data/")
 os.getcwd()
 
-playerData= pd.read_excel("NBA Player Game Log Data.xlsx")
+playerData= pd.read_csv("NBA Player Game Log Data.csv")
 
-prePlayer= playerData[playerData.Date<"07/30/2020"]
-#print(prePlayer)
-postPlayer= playerData[playerData.Date>'07/30/2020']
-#print(postPlayer)
+del playerData['Unnamed: 13']
+del playerData['Unnamed: 14']
 
-players= ['Aaron Gordon','CJ McCollum','Dennis Schroder','Donovan Mitchell','Dorian Finney-Smith','Doug McDermott','Evan Fournier','Jakob Poeltl','Jarrett Allen','Joe Ingles','Montrezl Harrell','Myles Turner','Nerlens Noel','Rudy Gay','Terrence Ross']
+print(playerData)
 
-playerNominal=['Season','Player','Date','Home/Away','Opp','Win/Loss','Tm']
+playerData= playerData.dropna()
 
+print(playerData)
+
+playerNominal=['Season','Player','Date','Home/Away','Opp','Win/Loss']
+
+playerNumeric= playerData
+
+for nom in playerNominal:
+    del playerNumeric[nom]
+
+
+print(playerNumeric)
+
+corrMatrix= playerNumeric.corr()
+
+print(corrMatrix)
 
 import seaborn as sn
 import matplotlib.pyplot as plt
 
-
-for player in players :
-    playerData= pd.read_excel("NBA Player Game Log Data.xlsx")
-    prePlayer= playerData[playerData.Date<"07/30/2020"]
-    postPlayer= playerData[playerData.Date>'07/30/2020']
-    individualPre= prePlayer[prePlayer['Player']== player]
-    individualPost= postPlayer[postPlayer['Player']== player]
-    preNumeric= individualPre
-    postNumeric= individualPost
-    for nom in playerNominal:
-        del preNumeric[nom]
-        del postNumeric[nom]
-        #del postNumeric['Attendance']
-    imatrixPre= preNumeric.corr()
-    imatrixPost= postNumeric.corr()
-    print(imatrixPost)
-    print(imatrixPre)
-    sn.heatmap(imatrixPost, annot=True)
-    plt.title("Post COVID Correlation Table for" + " " + player)
-    plt.show()
-    sn.heatmap(imatrixPre, annot=True)
-    plt.title("Pre COVID Correlation Table for" + " " + player)
-    plt.show()
-    
-
-
-
-
-
+sn.heatmap(corrMatrix, annot=True)
+plt.show()

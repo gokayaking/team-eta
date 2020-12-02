f19fa746aded2cb656cd5fbfa096250c2320cf15
gtrail5 <73748234+gtrail5@users.noreply.github.com>
2020-12-02 00:55:31 -0500

    Charts for Every Player

--- Analyses/PlayerCorrTable.py
+++ Analyses/PlayerCorrTable.py
@@ -19,45 +19,44 @@ os.getcwd()
 playerData= pd.read_excel("NBA Player Game Log Data.xlsx")
 
 prePlayer= playerData[playerData.Date<"07/30/2020"]
-print(prePlayer)
+#print(prePlayer)
 postPlayer= playerData[playerData.Date>'07/30/2020']
-print(postPlayer)
-"""del playerData['Unnamed: 13']
-del playerData['Unnamed: 14']"""
-
-"""print(playerData)"""
-
-playerData= playerData.dropna()
-prePlayer= prePlayer.dropna()
-postPlayer= postPlayer.dropna()
-
-print(playerData)
-print(prePlayer)
-print(postPlayer)
+#print(postPlayer)
 
 players= ['Aaron Gordon','CJ McCollum','Dennis Schroder','Donovan Mitchell','Dorian Finney-Smith','Doug McDermott','Evan Fournier','Jakob Poeltl','Jarrett Allen','Joe Ingles','Montrezl Harrell','Myles Turner','Nerlens Noel','Rudy Gay','Terrence Ross']
 
 playerNominal=['Season','Player','Date','Home/Away','Opp','Win/Loss','Tm']
 
-#playerNumeric= playerData
-preNumeric= prePlayer
-#postNumeric= postPlayer
-
-for nom in playerNominal:
-   # del playerNumeric[nom]
-    del preNumeric[nom]
-   # del postNumeric[nom]
-del preNumeric['Tm']
-
-print(preNumeric)
-
-#corrMatrix= playerNumeric.corr()
-preMatrix= preNumeric.corr()
-
-print(preMatrix)
 
 import seaborn as sn
 import matplotlib.pyplot as plt
 
-sn.heatmap(preMatrix, annot=True)
-plt.show()
+
+for player in players :
+    playerData= pd.read_excel("NBA Player Game Log Data.xlsx")
+    prePlayer= playerData[playerData.Date<"07/30/2020"]
+    postPlayer= playerData[playerData.Date>'07/30/2020']
+    individualPre= prePlayer[prePlayer['Player']== player]
+    individualPost= postPlayer[postPlayer['Player']== player]
+    preNumeric= individualPre
+    postNumeric= individualPost
+    for nom in playerNominal:
+        del preNumeric[nom]
+        del postNumeric[nom]
+        #del postNumeric['Attendance']
+    imatrixPre= preNumeric.corr()
+    imatrixPost= postNumeric.corr()
+    print(imatrixPost)
+    print(imatrixPre)
+    sn.heatmap(imatrixPost, annot=True)
+    plt.title("Post COVID Correlation Table for" + " " + player)
+    plt.show()
+    sn.heatmap(imatrixPre, annot=True)
+    plt.title("Pre COVID Correlation Table for" + " " + player)
+    plt.show()
+    
+
+
+
+
+

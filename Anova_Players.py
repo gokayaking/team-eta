25ec4f372fbfaa1e3a52b8a9237a3a7c1604458a
jhoffs <73670820+jhoffs@users.noreply.github.com>
2020-12-02 14:52:18 -0500

    Revert "Revert "Merge branch 'main' of https://github.com/gokayaking/team-eta into main""
    This reverts commit 423271bd31b77a65119b729442782b393f4e19a3.

--- Anova_Players.py
+++ Anova_Players.py
@@ -10,15 +10,13 @@ import scipy.stats as stats
 
 df = pd.read_excel("data/NBA Player Game Log Data.xlsx")
 df = df.sort_values(by=['Season','Date'], ascending = False)
-df = df.drop(df.columns[[0, 1, 2, 3, 4, 5]], axis=1)
 df = df.reset_index(drop = True)
+df = df.drop(df.columns[[0, 1, 2, 3, 4, 5]], axis=1)
+df = df.dropna()
 
-df_2019 = df.iloc[1161:, :]
-df_2019 = df_2019.dropna()
-df_pre = df.iloc[194:1161, :]
-df_pre = df_pre.dropna()
+df_2019 = df.iloc[1162:, :]
+df_pre = df.iloc[194:1162, :]
 df_post = df.iloc[:194, :]
-df_post = df_post.dropna()
 
 result = stats.f_oneway(df_2019, df_pre, df_post)
 
@@ -26,20 +24,22 @@ result = stats.f_oneway(df_2019, df_pre, df_post)
 stats = ['FG%', 'FT', 'FTA', 'FT%', 'TS%', 'eFG%', 'DRtg']
 
 for i in range(7):
-   print(stats[i] + ': ' + 'F-ratio: ' + str(result[0][i]) + ' ' + 'P-value: ' + str(result[1][i]))
+    print('Stat: ' + stats[i])
+    print('F: ' + str(result[0][i]))
+    print('P Value: ' + str(result[1][i]) + "\n")
     
 import matplotlib.pyplot as plt
     
 left = [1, 2, 3, 4, 5, 6, 7]
 
-plt.bar(left, result[1], tick_label = stats, width = .8)
+plt.bar(left, result[1], tick_label = stats, width = .8, color = ['red', 'green', 'green', 'red', 'red', 'red', 'green'])
 plt.xlabel('Statistics')
 plt.ylabel('P Value')
 plt.title('NBA Player ANOVA Test P Values')
 
 plt.show()
 
-plt.bar(left, result[0], tick_label = stats, width = .8)
+plt.bar(left, result[0], tick_label = stats, width = .8, color = ['red', 'green', 'green', 'red', 'red', 'red', 'green'])
 plt.xlabel('Statistics')
 plt.ylabel('F Value')
 plt.title('NBA Team ANOVA Test F Values')
